4be2ebb7eb1f05d9f1107c9c0c2118b246b0457a
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2019-12-11 23:01:55 +0100

    Write doc comments

--- pimu/pimu.py
+++ pimu/pimu.py
@@ -1,7 +1,10 @@
 """Read Gyro and Accelerometer by Interfacing Raspberry Pi
-with MPU6050 using Python. See: www.electronicwings.com
+with MPU6050 using Python.
 
-https://43zrtwysvxb2gf29r5o0athu-wpengine.netdna-ssl.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
+See: www.electronicwings.com
+
+Registers, addresses and instructions documentation at:
+    https://43zrtwysvxb2gf29r5o0athu-wpengine.netdna-ssl.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
 """
 import smbus
 from time import sleep
@@ -21,19 +24,61 @@ GYRO_ZOUT_H = 0x47
 
 
 def MPU_Init():
-    # write to sample rate register
+    # Sample Rate Divider.
+    # Specifies the divider from the gyroscope output rate used to generate
+    # the Sample Rate.
+    #   Sample Rate = Gyroscope Output Rate / (1 + SMPLRT_DIV)
+    # where Gyroscope Output Rate = 8kHz when the DLPF is disabled
+    # (DLPF_CFG = 0 or 7), and 1kHz when the DLPF is enabled (see Register 26).
     bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
 
-    # Write to power management register
+    # Power Management 1.
+    # This register allows the user to configure the power mode
+    # and clock source. It also provides a bit for resetting the entire device
+    # and a bit for disabling the temperature sensor.
+    # There are 3 clock sources:
+    # * internal 8MHz oscillator (CLKSEL = 0)
+    # * gyroscope based clock (recommended, CLKSEL = 1-3 for x, y, z
+    #   gyro reference)
+    # * external source (CLKSEL = 4-5)
+    # CLKSEL 6 and 7 are other things.
+    # Bits 0-2 configure CLKSEL. Bits 3-7 configure other things.
+    # Here we take the x gyroscope as clock reference.
     bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
 
-    # Write to Configuration register
+    # Configuration.
+    # This register configures the external Frame Synchronization (FSYNC)
+    # pin sampling (bits 3-5) and the Digital Low Pass Filter (DLPF) setting
+    # (bits 0-2) for both the gyroscopes and accelerometers. Bits 6-7 unused.
+    # The accelerometer and gyroscope are filtered according to the value
+    # of DLPF_CFG.
+    # By choosing DLPF_CFG = 0 we are setting the following:
+    # * Accelerometer Bandwidth = 160 Hz (max)
+    # * Accelerometer Delay = 0 ms (min)
+    # * Gyroscope Bandwidth = 256 Hz (max)
+    # * Gyroscope Delay = 0.98 ms (min)
+    # * Gyroscope Fs = 8 kHz (max)
+    # Bandwidth is the vibration the accelerometer can detect. It is filter by
+    # a filter with bandwidth set by DLPF_CFG.
     bus.write_byte_data(Device_Address, CONFIG, 0)
 
-    # Write to Gyro configuration register
+    # Gyroscope Configuration.
+    # This register is used to trigger gyroscope self-test and configure
+    # the gyroscopes’ full scale range.
+    # 24 = 0 0 0 1 1 0 0 0
+    # Bits 3-4 set FS_SEL, which selects the full scale range of the gyroscope
+    # outputs. The full scale range is the maximum angular velocity that the
+    # gyro can read. FS_SEL = 3 sets +-2000 degree/s.
     bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
 
-    # Write to interrupt enable register
+    # Interrupt Enable.
+    # This register enables interrupt generation by interrupt sources.
+    # Bit 0, which we set to 1, is DATA_RDY_EN.
+    # DATA_RDY_EN enables the Data Ready interrupt, which occurs each time
+    # a write operation to all of the sensor registers has been completed.
+    # To my understanding, when we write data to all sensor registers, we set
+    # bit 1 of register INT_STATUS (3A) to 1. Such bit is set back to 0 after
+    # a read operation.
     bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
 
