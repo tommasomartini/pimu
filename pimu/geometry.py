Bugfix in rotation matrix construction

pimu/geometry.py
@@ -2,8 +2,8 @@
 
 Every time we deal with a rotation, we adopt one of the following (equivalent)
 conventions:
-* intrinsic rotations: x - y'- z" (yaw - pitch - roll)
-* extrinsic rotations: z - y - x
+* intrinsic rotations: z - y'- x" (yaw - pitch - roll)
+* extrinsic rotations: x - y - z
 
 The relation between axes and Tait-Bryan angles here used holds if the axes
 are so oriented:
@@ -41,20 +41,20 @@ def build_rotation_matrix(yaw_rad, pitch_rad, roll_rad):
     """Builds and returns a rotation matrix.
 
     The applied rotation of the object in the reference system is the series
-    of intrinsic rotations: x - y'- z".
+    of intrinsic rotations: z - y'- x".
 
     Args:
-        yaw_rad (float): Rotation around the X axis in radians.
+        yaw_rad (float): Rotation around the Z axis in radians.
         pitch_rad (float): Rotation around the Y axis in radians.
-        roll_rad (float): Rotation around the Z axis in radians.
+        roll_rad (float): Rotation around the X axis in radians.
 
     Returns:
         A numpy array with shape (3, 3).
     """
     yaw_matrix = np.array([
-        [1, 0, 0],
-        [0, np.cos(yaw_rad), -np.sin(yaw_rad)],
-        [0, np.sin(yaw_rad), np.cos(yaw_rad)],
+        [np.cos(yaw_rad), -np.sin(yaw_rad), 0],
+        [np.sin(yaw_rad), np.cos(yaw_rad), 0],
+        [0, 0, 1],
     ])
     pitch_matrix= np.array([
         [np.cos(pitch_rad), 0, np.sin(pitch_rad)],
@@ -62,10 +62,11 @@ def build_rotation_matrix(yaw_rad, pitch_rad, roll_rad):
         [-np.sin(pitch_rad), 0, np.cos(pitch_rad)],
     ])
     roll_matrix = np.array([
-        [np.cos(roll_rad), -np.sin(roll_rad), 0],
-        [np.sin(roll_rad), np.cos(roll_rad), 0],
-        [0, 0, 1],
+        [1, 0, 0],
+        [0, np.cos(roll_rad), -np.sin(roll_rad)],
+        [0, np.sin(roll_rad), np.cos(roll_rad)],
     ])
+
     rotation_matrix = yaw_matrix @ pitch_matrix @ roll_matrix
     return rotation_matrix
 

pimu/geometry_tests.py
@@ -37,9 +37,9 @@ class BuildRotationMatrixTest(unittest.TestCase):
         pitch_rad = 0
         roll_rad = 0
         expected_rotation_matrix = np.array([
-            [1, 0, 0],
-            [0, _SQRT3 / 2, -0.5],
-            [0, 0.5, _SQRT3 / 2],
+            [_SQRT3 / 2, -0.5, 0],
+            [0.5, _SQRT3 / 2, 0],
+            [0, 0, 1],
         ])
 
         rotation_matrix = \
@@ -69,9 +69,9 @@ class BuildRotationMatrixTest(unittest.TestCase):
         pitch_rad = 0
         roll_rad = np.deg2rad(30)
         expected_rotation_matrix = np.array([
-            [_SQRT3 / 2, -0.5, 0],
-            [0.5, _SQRT3 / 2, 0],
-            [0, 0, 1],
+            [1, 0, 0],
+            [0, _SQRT3 / 2, -0.5],
+            [0, 0.5, _SQRT3 / 2],
         ])
 
         rotation_matrix = \
@@ -85,9 +85,9 @@ class BuildRotationMatrixTest(unittest.TestCase):
         pitch_rad = np.deg2rad(45)
         roll_rad = np.deg2rad(10)
         expected_rotation_matrix = np.array([
-            [0.6963642, -0.1227878,  0.7071068],
-            [0.4985659,  0.7914746, -0.3535534],
-            [-0.5162450,  0.5987412,  0.6123725],
+            [0.6123725, -0.3860665,  0.6898932],
+            [0.3535534,  0.9142624,  0.1977984],
+            [-0.7071068,  0.1227878,  0.6963642],
         ])
 
         rotation_matrix = \