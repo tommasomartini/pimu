86251cff041b60c12559fac928955c7c3a11a124
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-01-19 14:34:04 +0100

    More tests for accel data to Tait-Bryan

--- pimu/sensorboard_tests.py
+++ pimu/sensorboard_tests.py
@@ -5,6 +5,7 @@ import numpy as np
 import pimu.sensorboard as sb
 
 _PI = np.pi
+_SQRT3 = np.sqrt(3)
 
 
 class AccelerometerDataToConventionalSystemTest(unittest.TestCase):
@@ -30,14 +31,43 @@ class AccelerometerDataToTaitbryanTest(unittest.TestCase):
         expected_yaw = 0
         expected_pitch = 0
         expected_roll = 0
-        expected_output = (expected_yaw, expected_pitch, expected_roll)
 
         yaw, pitch, roll = sb.accelerometer_data_to_taitbryan(acc_x=acc_x,
                                                               acc_y=acc_y,
                                                               acc_z=acc_z)
-        output = (yaw, pitch, roll)
+        self.assertAlmostEqual(expected_yaw, yaw, places=7)
+        self.assertAlmostEqual(expected_pitch, pitch, places=7)
+        self.assertAlmostEqual(expected_roll, roll, places=7)
 
-        self.assertTupleEqual(expected_output, output)
+    def test_only_roll_applied(self):
+        acc_x = 0
+        acc_y = 0.5
+        acc_z = _SQRT3 / 2
+        expected_yaw = 0
+        expected_pitch = 0
+        expected_roll = np.deg2rad(30)
+
+        yaw, pitch, roll = sb.accelerometer_data_to_taitbryan(acc_x=acc_x,
+                                                              acc_y=acc_y,
+                                                              acc_z=acc_z)
+        self.assertAlmostEqual(expected_yaw, yaw, places=7)
+        self.assertAlmostEqual(expected_pitch, pitch, places=7)
+        self.assertAlmostEqual(expected_roll, roll, places=7)
+
+    def test_only_pitch_applied(self):
+        acc_x = -0.5
+        acc_y = 0
+        acc_z = _SQRT3 / 2
+        expected_yaw = 0
+        expected_pitch = np.deg2rad(30)
+        expected_roll = 0
+
+        yaw, pitch, roll = sb.accelerometer_data_to_taitbryan(acc_x=acc_x,
+                                                              acc_y=acc_y,
+                                                              acc_z=acc_z)
+        self.assertAlmostEqual(expected_yaw, yaw, places=7)
+        self.assertAlmostEqual(expected_pitch, pitch, places=7)
+        self.assertAlmostEqual(expected_roll, roll, places=7)
 
     def test_board_lying_on_right_side(self):
         acc_x = 0
@@ -46,14 +76,43 @@ class AccelerometerDataToTaitbryanTest(unittest.TestCase):
         expected_yaw = 0
         expected_pitch = 0
         expected_roll = _PI / 2
-        expected_output = (expected_yaw, expected_pitch, expected_roll)
 
         yaw, pitch, roll = sb.accelerometer_data_to_taitbryan(acc_x=acc_x,
                                                               acc_y=acc_y,
                                                               acc_z=acc_z)
-        output = (yaw, pitch, roll)
+        self.assertAlmostEqual(expected_yaw, yaw, places=7)
+        self.assertAlmostEqual(expected_pitch, pitch, places=7)
+        self.assertAlmostEqual(expected_roll, roll, places=7)
 
-        self.assertTupleEqual(expected_output, output)
+    def test_board_lying_on_bottom_side(self):
+        acc_x = -1
+        acc_y = 0
+        acc_z = 0
+        expected_yaw = 0
+        expected_pitch = _PI / 2
+        expected_roll = 0
+
+        yaw, pitch, roll = sb.accelerometer_data_to_taitbryan(acc_x=acc_x,
+                                                              acc_y=acc_y,
+                                                              acc_z=acc_z)
+        self.assertAlmostEqual(expected_yaw, yaw, places=7)
+        self.assertAlmostEqual(expected_pitch, pitch, places=7)
+        self.assertAlmostEqual(expected_roll, roll, places=7)
+
+    # def test_no_faces_parallel_to_ground(self):
+    #     acc_x = -0.5
+    #     acc_y = _SQRT3 / 4
+    #     acc_z = (_SQRT3 / 2) ** 2
+    #     expected_yaw = 0
+    #     expected_pitch = np.deg2rad(30)
+    #     expected_roll = np.deg2rad(30)
+    #
+    #     yaw, pitch, roll = sb.accelerometer_data_to_taitbryan(acc_x=acc_x,
+    #                                                           acc_y=acc_y,
+    #                                                           acc_z=acc_z)
+    #     self.assertAlmostEqual(expected_yaw, yaw, places=7)
+    #     self.assertAlmostEqual(expected_pitch, pitch, places=7)
+    #     self.assertAlmostEqual(expected_roll, roll, places=7)
 
 
 if __name__ == '__main__':
