895111461c7c9d63103b66f7ec24078646231ce5
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-01-19 15:02:37 +0100

    Debugger plots correctly

--- pimu/debugger.py
+++ pimu/debugger.py
@@ -11,24 +11,16 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 import pimu.constants as const
 import pimu.geometry as geom
-
-
-def _fake_data_generator():
-    rate = 60
-    num_samples = 500
-    max_deg = 1
-    np.random.seed(0)
-    for idx in range(num_samples):
-        time.sleep(1. / rate)
-        yaw_rad, roll_rad, pitch_rad = np.deg2rad(max_deg * np.random.rand(3))
-        yield yaw_rad, roll_rad, pitch_rad
+import pimu.sensorboard as sb
 
 
 class Board:
+    """Represents a board to visualize that is always aligned with the sensor.
+    """
 
-    # Dimensions of the board.
-    _width = const.BOARD_WIDTH_mm       # along x
-    _length = const.BOARD_LENGTH_mm     # along y
+    # Dimensions of the board along the board's axes.
+    _length = const.BOARD_LENGTH_mm     # along x
+    _width = const.BOARD_WIDTH_mm       # along y
     _height = const.BOARD_HEIGHT_mm     # along z
 
     # How bigger the visualization box is, with respect to the maximum board
@@ -40,7 +32,8 @@ class Board:
     _axes_scale = 2.
 
     def __init__(self):
-        dimensions = np.array([self._width, self._length, self._height])
+        # Dimensions along the board's X, Y and Z axes.
+        dimensions = np.array([self._length, self._width, self._height])
         half_dimensions = dimensions / 2
         signs = [-1, 1]
         vertices_coords = np.c_[half_dimensions] @ np.c_[signs].T
@@ -51,14 +44,10 @@ class Board:
         # X, Y and Z axes as column vectors.
         self._init_board_axes = np.diag(self._axes_scale * half_dimensions)
 
-        self._yaw_rad = 0
-        self._roll_rad = 0
-        self._pitch_rad = 0
-
         self._vertices = self._init_vertices
         self._board_axes = self._init_board_axes
 
-    def rotate(self, yaw_rad, roll_rad, pitch_rad):
+    def rotate(self, yaw_rad, pitch_rad, roll_rad):
         """Rotates the board from its current orientation.
 
         The rotation follows one of the following two (equivalent) conventions:
@@ -67,26 +56,30 @@ class Board:
 
         Args:
             yaw_rad (float): Rotation around the Z axis in radians.
-            roll_rad (float): Rotation around the Y axis in radians.
-            pitch_rad (float): Rotation around the X axis in radians.
+            pitch_rad (float): Rotation around the Y axis in radians.
+            roll_rad (float): Rotation around the X axis in radians.
         """
-
-        # Update the current orientation.
-        self._yaw_rad += yaw_rad
-        self._roll_rad += roll_rad
-        self._pitch_rad += pitch_rad
-
         # Build the rotation matrix.
-        rotation_matrix = geom.build_rotation_matrix(yaw_rad=self._yaw_rad,
-                                                     roll_rad=self._roll_rad,
-                                                     pitch_rad=self._pitch_rad)
+        rotation_matrix = geom.build_rotation_matrix(yaw_rad=yaw_rad,
+                                                     pitch_rad=pitch_rad,
+                                                     roll_rad=roll_rad)
+
+        # Matplotlib's reference system for drawing a plot has the Z axis
+        # pointing upwards. The following concatenation only serves for
+        # data visualization.
+        plot_adapting_rotation_matrix = np.array([
+            [0, 1, 0],
+            [1, 0, 0],
+            [0, 0, -1],
+        ]).T
+        rotation_matrix = plot_adapting_rotation_matrix @ rotation_matrix
 
         self._vertices = rotation_matrix @ self._init_vertices
         self._board_axes = rotation_matrix @ self._init_board_axes
 
     def _plot_parallelepiped(self, ax):
         # "n" stands for "negative" and "p" for positive.
-        nnn, nnp, npn, npp, pnn, pnp, ppn, ppp = self._vertices.T
+        nnp, nnn, pnp, pnn, npp, npn, ppp, ppn = self._vertices.T
 
         top_face = [nnp, npp, ppp, pnp]
         bottom_face = [nnn, npn, ppn, pnn]
@@ -142,28 +135,33 @@ class Board:
 def main():
     board = Board()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection=Axes3D.name)
-    ax.set_xlabel('X axis')
-    ax.set_ylabel('Y axis')
-    ax.set_zlabel('Z axis')
-
-    UDP_IP = "127.0.0.1"
+    UDP_IP = "192.168.1.128"
     UDP_PORT = 5005
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
 
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection=Axes3D.name)
+
     while True:
         data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-
         data_dict = json.loads(data.decode('utf-8'))
-        yaw, roll, pitch = map(float, [data_dict['yaw'],
-                                       data_dict['roll'],
-                                       data_dict['pitch']])
+        acc_x, acc_y, acc_z = map(float, [data_dict['acc_x'],
+                                          data_dict['acc_y'],
+                                          data_dict['acc_z']])
+
+        acc_x, acc_y, acc_z = \
+            sb.accelerometer_data_to_conventional_system(acc_x, acc_y, acc_z)
+        yaw, pitch, roll = sb.accelerometer_data_to_taitbryan(acc_x=acc_x,
+                                                              acc_y=acc_y,
+                                                              acc_z=acc_z)
 
         ax.clear()
-        board.rotate(yaw_rad=yaw, roll_rad=roll, pitch_rad=pitch)
+        board.rotate(yaw_rad=yaw, pitch_rad=pitch, roll_rad=roll)
         board.plot(ax)
+        ax.set_xlabel('X axis')
+        ax.set_ylabel('Y axis')
+        ax.set_zlabel('Z axis')
         plt.pause(1e-5)
 
     plt.close()
