4ec93485032fc2a1e5e147a50bc242965b7c71b7
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-02-26 21:37:43 +0100

    First draft MPU6050 class

---
+++ pimu/mpu6050/mpu6050.py
@@ -0,0 +1,70 @@
+import logging
+import smbus
+
+import pimu.mpu6050.constants as const
+import pimu.mpu6050.initialization as init
+import pimu.mpu6050.registers as regs
+import pimu.mpu6050.sensor as sensor
+
+_logger = logging.getLogger(__name__)
+
+
+class MPU6050:
+
+    def __init__(self, gyro_sensitivity, acc_sensitivity, rate_hz):
+        self._gyro_sensitivity = const.GYRO_SENSITIVITY[gyro_sensitivity]
+        self._acc_sensitivity = const.ACCEL_SENSITIVITY[acc_sensitivity]
+        self._rate_hz = rate_hz
+
+        # The argument is 0 for older versions of the board.
+        self._bus = smbus.SMBus(1)
+        self._device_address = regs.MPU6050_ADDRESS
+
+        gyro_full_scale_range = const.FS_SEL[gyro_sensitivity]
+        acc_full_scale_range = const.AFS_SEL[acc_sensitivity]
+        init.initialize(self._bus,
+                        self._device_address,
+                        gyro_full_scale_range=gyro_full_scale_range,
+                        acc_full_scale_range=acc_full_scale_range)
+
+        _logger.info('{} initialized'.format(self.__class__.__name__))
+
+        _logger.debug('Gyroscope sensitivity {} deg/s'.format(gyro_sensitivity))
+        _logger.debug('Accelerometer sensitivity: {}'.format(acc_sensitivity))
+        _logger.debug('Rate: {} Hz'.format(self._rate_hz))
+
+        # Bias that can be removed through calibration.
+        self._acc_x_bias = 0
+        self._acc_y_bias = 0
+        self._acc_z_bias = 0
+
+        self._gyro_x_bias = 0
+        self._gyro_y_bias = 0
+        self._gyro_z_bias = 0
+
+    def read_next(self):
+        accelerometer_data = \
+            sensor.read_accelerometer_data(bus=self._bus,
+                                           device_address=self._device_address,
+                                           sensitivity=self._acc_sensitivity)
+
+        temperature_deg = \
+            sensor.read_temperature_data(bus=self._bus,
+                                         device_address=self._device_address)
+
+        gyroscope_data = \
+            sensor.read_gyroscope_data(bus=self._bus,
+                                       device_address=self._device_address,
+                                       sensitivity=self._gyro_sensitivity)
+
+        acc_x, acc_y, acc_z = accelerometer_data
+        acc_x -= self._acc_x_bias
+        acc_y -= self._acc_y_bias
+        acc_z -= (self._acc_z_bias + 1)
+
+        gyro_x, gyro_y, gyro_z = gyroscope_data
+        gyro_x -= self._gyro_x_bias
+        gyro_y -= self._gyro_y_bias
+        gyro_z -= self._gyro_z_bias
+
+        return acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, temperature_deg
