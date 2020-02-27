41137d895ff1c14a59e90d81178fc5cda3a8a92a
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-02-27 19:59:40 +0100

    Add Imu's yaw pitch roll reading function

--- pimu/imu.py
+++ pimu/imu.py
@@ -1,7 +1,10 @@
 import logging
+import time
 
 import numpy as np
 from tqdm import tqdm
+
+import pimu.sensorboard as sb
 
 _logger = logging.getLogger(__name__)
 
@@ -11,11 +14,15 @@ class Imu:
     NUMBER_OF_CALIBRATION_SAMPLES = 1000
 
     def __init__(self):
+        self._yaw = 0
+        self._pitch = 0
+        self._roll = 0
+        self._prev_time_ms = int(round(time.time() * 1000))
+
         # Bias that can be removed through calibration.
         self._acc_x_bias = 0
         self._acc_y_bias = 0
         self._acc_z_bias = 0
-
         self._gyro_x_bias = 0
         self._gyro_y_bias = 0
         self._gyro_z_bias = 0
@@ -23,6 +30,25 @@ class Imu:
     def read_next(self):
         raise NotImplementedError('Only derived classes are supposed '
                                   'to implement this function')
+
+    def read_yaw_pitch_roll(self):
+        acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, *_ = self.read_next()
+        curr_time_ms = int(round(time.time() * 1000))
+        delta_time_ms = curr_time_ms - self._prev_time_ms
+        self._prev_time_ms = curr_time_ms
+
+        acc_pitch, acc_roll = \
+            sb.pitch_and_roll_from_accelerometer_data(acc_x, acc_y, acc_z)
+
+        gyro_yaw_delta, gyro_pitch_delta, gyro_roll_delta = \
+            sb.gyroscope_data_to_taitbryan_deltas(gyro_x, gyro_y, gyro_z,
+                                                  delta_time_ms=delta_time_ms)
+
+        self._yaw += gyro_yaw_delta
+        self._pitch = 0.5 * acc_pitch + 0.5 * (self._pitch + gyro_pitch_delta)
+        self._roll = 0.5 * acc_roll + 0.5 * (self._roll + gyro_roll_delta)
+
+        return self._yaw, self._pitch, self._roll
 
     def calibrate(self):
         input('Place the IMU on a flat surface and press '
